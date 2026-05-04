331524d569d5e7d6e60619d38903151b0a4cc6e7
sivakovjr <jonsivakov@gmail.com>
2026-05-04 19:44:25 +0300

    completed task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
-module.exports = function towelSort(/* matrix */) {
-  throw new Error('Not implemented');
+module.exports = function towelSort(matrix) {
+  if (!matrix || matrix.length === 0) {
+    return [];
+  }
+
+  return matrix.reduce((acc, row, index) => {
+    // Если индекс строки нечетный (1, 3...), разворачиваем её
+    const sortedRow = index % 2 !== 0 ? row.reverse() : row;
+    return acc.concat(sortedRow);
+  }, []);
 };
